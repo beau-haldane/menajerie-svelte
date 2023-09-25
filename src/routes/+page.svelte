8ed88bf584ce28ef610a0158164633cb162915a2
Beau Jorgensen <beau@Beaus-Laptop.gateway>
2023-09-25 21:00:16 +1000

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<!-- Hero/Nav -->
+<div class="h-screen w-screen flex flex-col items-center">
+  <!-- Curved DIVs - do not place content inside -->
+  <div class='curved-bottom-border-hero'></div>
+  <div class='curved-bottom-border-hero outline-01'></div>
+  <div class='curved-bottom-border-hero outline-02'></div>
+  <div class='curved-bottom-border-hero outline-03'></div>
+
+  <!-- Hero Container -->
+  <div class="hero-container">
+    <h1>Menajerie</h1>
+  </div>
+</div>
+
+<style>
+  h1 {
+  color: #F6F1EB;
+  font-size: 2.5em;
+  font-family: 'Caprasimo';
+}
+
+/* Curved-bottom Hero BG Styling */
+.curved-bottom-border-hero {
+  position: absolute;
+  top: 0;
+  left: -50%;
+  height: 100%;
+  width: 200%;
+  border-radius: 0% 0% 50% 50%/0% 0% 50% 50%;
+  background-image: url("../img/DSC_9426.jpg");
+  background-position: center;
+  background-size: 65%;
+  background-position-y: 30%;
+  z-index: -10;
+}
+
+.outline-01 {
+  width: calc(200% + 6em);
+  height: calc(100% + 6em);
+  background-image: none;
+  background-color: #CA604F;
+  top: -3em;
+  left: calc(-50% - 3em);
+  z-index: -11;
+}
+
+.outline-02 {
+  width: calc(200% + 12em);
+  height: calc(100% + 12em);
+  background-image: none;
+  background-color: #E68953;
+  top: -6em;
+  left: calc(-50% - 6em);
+  z-index: -12;
+}
+
+.outline-03 {
+  width: calc(200% + 18em);
+  height: calc(100% + 18em);
+  background-image: none;
+  background-color: #F0AC5D;
+  top: -9em;
+  left: calc(-50% - 9em);
+  z-index: -13;
+}
+
+/* Hero */
+.hero-container {
+  width: 90em;
+  height: 90%;
+}
+</style>

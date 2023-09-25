09354a8ea8e16275ef01c913385573861fcb3822
Beau Jorgensen <beau@Beaus-Laptop.gateway>
2023-09-25 22:33:27 +1000

    pages build settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
+			base: '',
 		},
 		adapter: adapter({
 			fallback: '200.html' // may differ from host to host

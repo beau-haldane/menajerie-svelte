d2546ee144d4eb5b9437abaed2d065c3a40b4bd6
beau-haldane <beauhaldane@gmail.com>
2025-01-23 18:07:16 +1000

    update blurb

--- src/components/Blurb.svelte
+++ src/components/Blurb.svelte
@@ -1,26 +1,28 @@
-<div class="flex flex-col lg:flex-row items-center px-12 gap-12 w-9/12 sm:w-full">
+<div
+  class="flex flex-col lg:flex-row items-center px-12 gap-12 w-9/12 sm:w-full"
+>
   <div class="text-center lg:text-left lg:w-2/5">
     <h3>
-      Neo-soul making, no shit taking, gonna get your booty shaking Menajerie
-      are a 6-piece female led collective based in Meanjin / Brisbane.
+      Menajerie is a femme-driven neo soul collective from Meanjin, Brisbane.
     </h3>
   </div>
-  <div class="lg:w-3/5 hidden text-center lg:text-left lg:block flex flex-col gap-4">
+  <div
+    class="lg:w-3/5 hidden text-center lg:text-left lg:block flex flex-col gap-4"
+  >
     <p>
-      The group is fronted by songstress & ivory-tinkler Gracie Mack, powerhouse
-      vocalist Rosa Mack (Rosa Mack, KAYSO GRANDE) and fiery chanteuse Mel
-      Galletly (Willis & the Lovely Ones).
+      Led by three mesmerizing frontwomen - Gracie Mack, Melanie G, and Rosa
+      Mack - Menajerie weaves sultry harmonies with intricate songwriting and
+      refined soul.
     </p>
     <p>
-      Menajerie pens joyous, heartbreaking and honest originals with a strong
-      focus on tight vocal harmony and cosmic funk.
+      Their onstage presence enchants the listener into a hazy dream of warm
+      tones and hypnotising grooves. The dynamic trio are accompanied by the
+      cosmic tones of Andrew Fincher on electric guitar, Lee Brackenborough with
+      smouldering bass lines and Beau Jorgensen anchoring the rhythm.
     </p>
     <p>
-      With Andrew Fincher (Kerbside Collection, HRBRT, CityPiss) playing guitar,
-      Lee Brackenborough (Elktung, HRBRT, Big Dead) on bass and Beau Jorgensen
-      (Mover, Stormchasers) on drums providing a powerful rhythm section,
-      Menajerie leave their audience worshipping at a pussy shaped altar of
-      Funk.
+      This electrifying six-piece cultivates fierce, heart spilling energy that
+      uplifts the mind and emboldens the soul.
     </p>
   </div>
 </div>
